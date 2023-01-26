76c283da568e035a2c726af908955c624861c8bd
Shwetabh Singh <shwetabhsingh12345@gmail.com>
2023-01-26 22:01:05 +0530

    Created API Call function in views file

--- weather/home/views.py
+++ weather/home/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+import json
+import urllib.request
 # Create your views here.
 
 def index(request):
-    context = {
-        "variable":"HIIII SGSI"
-    }
-    return render(request, 'home.html',context)
+    if request.method == 'POST':
+        city = request.POST['city']
+        source = urllib.request.urlopen("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=569aa4697bdc9cb76feec2d87370aeef&units=metric").read()
+        list_of_data = json.loads(source)
+
+        data = {
+            "country_code" : str(list_of_data["sys"]["country"]),
+            "coordinates" : str(list_of_data["coord"]["lon"]) + ',' + str(list_of_data["coord"]["lat"]),
+            "temp" : str(list_of_data["main"]["temp"]) + "°C",
+            "pressure" : str(list_of_data["main"]["pressure"]),
+            "humidity" : str(list_of_data["main"]["humidity"]),
+            "main" : str(list_of_data["weather"][0]["main"]),
+            "description" : str(list_of_data["weather"][0]["description"]),
+            "icon" : list_of_data["weather"][0]["icon"]
+        }
