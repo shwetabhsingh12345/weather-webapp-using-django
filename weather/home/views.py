3cdcf0e1ed1e02cf1885df1415d7c9010aed9cc6
Shwetabh Singh <shwetabhsingh12345@gmail.com>
2023-01-29 18:00:28 +0530

    Website title is now dynamic

--- weather/home/views.py
+++ weather/home/views.py
@@ -5,11 +5,13 @@ import urllib.request
 
 def index(request):
     if request.method == 'POST':
+        global city
         city = request.POST['city']
         new_city = city.replace(" ","+")
         source = urllib.request.urlopen("https://api.openweathermap.org/data/2.5/weather?q=" + new_city + "&appid=569aa4697bdc9cb76feec2d87370aeef&units=metric").read()
         list_of_data = json.loads(source)
         data = {
+            "cityis" : " of "+city,
             "country_code" : str(list_of_data["sys"]["country"]),
             "coordinates" : str(list_of_data["coord"]["lon"]) + ',' + str(list_of_data["coord"]["lat"]),
             "temp" : str(list_of_data["main"]["temp"]) + "°C",
@@ -25,4 +27,7 @@ def index(request):
     return render(request,'home.html', data)
 
 def errorpage(request):
-    return render(request,'error500.html')
+    error_data = {
+        "error_city" : " of "+city+" not found"
+    }
+    return render(request,'error500.html',error_data)
